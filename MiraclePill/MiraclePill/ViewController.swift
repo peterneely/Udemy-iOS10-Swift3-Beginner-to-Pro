954305fca4156d6c34ccde77c0e0704a18503bd5
Peter Neely <Peter@DialMBPR.local>
2017-04-21 16:17:10 -0700

    Initial commit

---
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Peter Neely on 4/21/17.
+//  Copyright © 2017 Peter Neely. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       self.view.backgroundColor = UIColor.purple
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
